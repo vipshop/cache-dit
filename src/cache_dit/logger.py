999e3ed25cb273cfbcd4ef4f7ddb88f0493485aa
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-06-15 15:10:51 +0800

    init DBCache

--- src/cache_dit/logger.py
+++ src/cache_dit/logger.py
@@ -1,6 +1,3 @@
-# Adapted from:
-# https://github.com/skypilot-org/skypilot/blob/86dc0f6283a335e4aa37b3c10716f90999f48ab6/sky/sky_logging.py
-"""Logging configuration."""
 import logging
 import os
 import sys
