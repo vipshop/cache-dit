eb108e7979f099d044b265f964fa671ac6c550f8
DefTruth <qiustudent_r@163.com>
2025-06-19 09:03:31 +0000

    update example

---
+++ examples/run_flux_fill.py
@@ -0,0 +1,33 @@
+import torch
+from diffusers import FluxFillPipeline
+from diffusers.utils import load_image
+from cache_dit.cache_factory import apply_cache_on_pipe, CacheType
+
+pipe = FluxFillPipeline.from_pretrained(
+    "black-forest-labs/FLUX.1-Fill-dev",
+    torch_dtype=torch.bfloat16,
+).to("cuda")
+
+
+# Default options, F8B8, good balance between performance and precision
+cache_options = CacheType.default_options(CacheType.DBCache)
+
+apply_cache_on_pipe(pipe, **cache_options)
+
+# Load image
+cup_image = load_image("./data/cup.png")
+cup_mask = load_image("./data/cup_mask.png")
+
+image = pipe(
+    prompt="a white paper cup",
+            image=load_image("./data/cup.png"),
+            mask_image=load_image("./data/cup_mask.png"),
+            guidance_scale=30,
+            num_inference_steps=28,
+            max_sequence_length=512,
+            generator=torch.Generator("cuda").manual_seed(0),
+            output_type="pil",
+).images[0]
+
+print("Saving image to flux-fill.png")
+image.save("flux-fill.png")
