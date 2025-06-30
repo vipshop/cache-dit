3490be2514f55a5f5d1a77e87e38842272b90015
DefTruth <qiustudent_r@163.com>
2025-06-30 09:44:17 +0800

    fix taylorseer

--- tests/test_taylorseer.py
+++ tests/test_taylorseer.py
@@ -1,8 +1,44 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from cache_dit.cache_factory.taylorseer import TaylorSeer
 
-taylor_seer = TaylorSeer(n_derivatives=2, warmup_steps=2, skip_interval_steps=2)
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Test TaylorSeer approximation."
+    )
+    parser.add_argument(
+        "--n_derivatives",
+        "--order",
+        type=int,
+        default=2,
+        help="Number of derivatives to approximate.",
+    )
+    parser.add_argument(
+        "--warmup_steps",
+        "--warmup",
+        type=int,
+        default=2,
+        help="Number of warmup steps before approximation starts.",
+    )
+    parser.add_argument(
+        "--skip_interval_steps",
+        type=int,
+        default=2,
+        help="Interval of steps to skip for approximation.",
+    )
+    return parser.parse_args()
+
+
+args = get_args()
+
+
+taylor_seer = TaylorSeer(
+    n_derivatives=args.n_derivatives,
+    warmup_steps=args.warmup_steps,
+    skip_interval_steps=args.skip_interval_steps,
+)
 
 x_values = np.arange(0, 10, 0.1)
 y_true = x_values**2
@@ -15,10 +51,17 @@ for x in x_values:
     y_pred.append(y_approx)
     errors.append(abs(y - y_approx))
 
+
+save_path = f"taylorseer_approximation_order_{args.n_derivatives}.png"
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.plot(x_values, y_true, label="True $y=x^2$")
-plt.plot(x_values, y_pred, "--", label="TaylorSeer Approximation")
+plt.plot(
+    x_values,
+    y_pred,
+    "--",
+    label=f"TaylorSeer Approximation, Order={args.n_derivatives}",
+)
 plt.legend()
 plt.xlabel("x")
 plt.ylabel("y")
@@ -34,5 +77,5 @@ plt.title("Approximation Error")
 plt.grid()
 
 plt.tight_layout()
-plt.savefig("taylorseer_approximation_test.png")
-print("Test completed and saved as 'taylorseer_approximation_test.png'.")
+plt.savefig(save_path)
+print(f"Test completed and saved as {save_path}.")
