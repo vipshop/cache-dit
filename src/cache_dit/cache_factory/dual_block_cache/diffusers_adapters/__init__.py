5493737677da5abc383b3d25f095f41159564ba7
DefTruth <qiustudent_r@163.com>
2025-08-18 05:16:12 +0000

    add enable_cache api

--- src/cache_dit/cache_factory/dual_block_cache/diffusers_adapters/__init__.py
+++ src/cache_dit/cache_factory/dual_block_cache/diffusers_adapters/__init__.py
@@ -1,9 +1,22 @@
 import importlib
+import functools
 
 from diffusers import DiffusionPipeline
+from cache_dit.cache_factory.dual_block_cache import cache_context
 
 
-def apply_db_cache_on_transformer(transformer, *args, **kwargs):
+def apply_db_cache_on_transformer(
+    transformer,
+    *,
+    residual_diff_threshold=0.05,
+    downsample_factor=1,
+    warmup_steps=0,
+    max_cached_steps=-1,
+    **kwargs,
+):
+    if getattr(transformer, "_is_cached", False):
+        return transformer
+
     transformer_cls_name: str = transformer.__class__.__name__
     if transformer_cls_name.startswith("Flux"):
         adapter_name = "flux"
@@ -26,11 +39,53 @@ def apply_db_cache_on_transformer(transformer, *args, **kwargs):
     apply_db_cache_on_transformer_fn = getattr(
         adapter_module, "apply_db_cache_on_transformer"
     )
-    return apply_db_cache_on_transformer_fn(transformer, *args, **kwargs)
+
+    # mock transformer_blocks -> cached_transformer_blocks
+    transformer = apply_db_cache_on_transformer_fn(transformer)
+
+    # create cache context
+    cache_kwargs, kwargs = cache_context.collect_cache_kwargs(
+        default_attrs={
+            "residual_diff_threshold": residual_diff_threshold,
+            "downsample_factor": downsample_factor,
+            "warmup_steps": warmup_steps,
+            "max_cached_steps": max_cached_steps,
+        },
+        **kwargs,
+    )
+    assert getattr(transformer, "_is_cached", False)
+
+    cached_forward = transformer.forward
+
+    @functools.wraps(cached_forward)
+    def new_cached_forward(
+        self,
+        *args,
+        **kwargs,
+    ):
+        with cache_context.cache_context(
+            cache_context.create_cache_context(
+                **cache_kwargs,
+            )
+        ):
+            return cached_forward(
+                *args,
+                **kwargs,
+            )
+
+    transformer.forward = new_cached_forward.__get__(transformer)
+
+    return transformer
 
 
-def apply_db_cache_on_pipe(pipe: DiffusionPipeline, *args, **kwargs):
+def apply_db_cache_on_pipe(
+    pipe: DiffusionPipeline,
+    *args,
+    **kwargs,
+):
     assert isinstance(pipe, DiffusionPipeline)
+    if getattr(pipe, "_is_cached", False):
+        return pipe
 
     pipe_cls_name: str = pipe.__class__.__name__
     if pipe_cls_name.startswith("Flux"):
--- src/cache_dit/cache_factory/dynamic_block_prune/diffusers_adapters/__init__.py
+++ src/cache_dit/cache_factory/dynamic_block_prune/diffusers_adapters/__init__.py
@@ -1,9 +1,22 @@
 import importlib
+import functools
 
 from diffusers import DiffusionPipeline
+from cache_dit.cache_factory.dynamic_block_prune import prune_context
 
 
-def apply_db_prune_on_transformer(transformer, *args, **kwargs):
+def apply_db_prune_on_transformer(
+    transformer,
+    *,
+    residual_diff_threshold=0.05,
+    downsample_factor=1,
+    warmup_steps=0,
+    max_pruned_steps=-1,
+    **kwargs,
+):
+    if getattr(transformer, "_is_pruned", False):
+        return transformer
+
     transformer_cls_name: str = transformer.__class__.__name__
     if transformer_cls_name.startswith("Flux"):
         adapter_name = "flux"
@@ -26,11 +39,52 @@ def apply_db_prune_on_transformer(transformer, *args, **kwargs):
     apply_db_prune_on_transformer_fn = getattr(
         adapter_module, "apply_db_prune_on_transformer"
     )
-    return apply_db_prune_on_transformer_fn(transformer, *args, **kwargs)
+    # mock transformer_blocks -> pruned_transformer_blocks
+    transformer = apply_db_prune_on_transformer_fn(transformer)
+
+    # create prune context
+    prune_kwargs, kwargs = prune_context.collect_prune_kwargs(
+        default_attrs={
+            "residual_diff_threshold": residual_diff_threshold,
+            "downsample_factor": downsample_factor,
+            "warmup_steps": warmup_steps,
+            "max_pruned_steps": max_pruned_steps,
+        },
+        **kwargs,
+    )
+    assert getattr(transformer, "_is_pruned", False)
+
+    pruned_forward = transformer.forward
+
+    @functools.wraps(pruned_forward)
+    def new_pruned_forward(
+        self,
+        *args,
+        **kwargs,
+    ):
+        with prune_context.prune_context(
+            prune_context.create_prune_context(
+                **prune_kwargs,
+            )
+        ):
+            return pruned_forward(
+                *args,
+                **kwargs,
+            )
+
+    transformer.forward = new_pruned_forward.__get__(transformer)
+
+    return transformer
 
 
-def apply_db_prune_on_pipe(pipe: DiffusionPipeline, *args, **kwargs):
+def apply_db_prune_on_pipe(
+    pipe: DiffusionPipeline,
+    *args,
+    **kwargs,
+):
     assert isinstance(pipe, DiffusionPipeline)
+    if getattr(pipe, "_is_pruned", False):
+        return pipe
 
     pipe_cls_name: str = pipe.__class__.__name__
     if pipe_cls_name.startswith("Flux"):
