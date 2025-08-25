e0f4ed311b61173b29ba71b22f1dcc2376ef44e8
DefTruth <qiustudent_r@163.com>
2025-08-25 02:08:42 +0000

    explict cache param on cache api

--- src/cache_dit/utils.py
+++ src/cache_dit/utils.py
@@ -33,7 +33,7 @@ def summary(pipe: DiffusionPipeline, details: bool = False):
     if hasattr(pipe, "_cache_options"):
         cache_options = pipe._cache_options
         cache_stats.cache_options = cache_options
-        print(f"\n🤗Cache Options: {pipe_cls_name}\n\n{cache_options}")
+        pprint(f"\n🤗Cache Options: {pipe_cls_name}\n\n{cache_options}")
 
     if hasattr(pipe.transformer, "_cached_steps"):
         cached_steps: list[int] = pipe.transformer._cached_steps
