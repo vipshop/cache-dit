2c03abad2d15bf8ba276691933a65d31a0a08663
DefTruth <qiustudent_r@163.com>
2025-08-28 06:25:08 +0000

    update bench

--- examples/run_qwen_image.py
+++ examples/run_qwen_image.py
@@ -81,8 +81,6 @@ if args.quantize:
             "norm_out",
             "proj_out",
         ],
-        weight_dtype=torch.float8_e4m3fn,
-        activation_dtype=torch.float8_e5m2,
     )
 
 if args.compile:
