6584979ef1f9302fcfd27158958a7ab3769edd2f
DefTruth <qiustudent_r@163.com>
2025-08-28 13:00:56 +0000

    support pattern_3_4_5

--- src/cache_dit/__init__.py
+++ src/cache_dit/__init__.py
@@ -24,3 +24,5 @@ Forward_Pattern_0 = ForwardPattern.Pattern_0
 Forward_Pattern_1 = ForwardPattern.Pattern_1
 Forward_Pattern_2 = ForwardPattern.Pattern_2
 Forward_Pattern_3 = ForwardPattern.Pattern_3
+Forward_Pattern_4 = ForwardPattern.Pattern_4
+Forward_Pattern_5 = ForwardPattern.Pattern_5
