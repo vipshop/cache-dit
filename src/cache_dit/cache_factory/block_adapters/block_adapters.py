d5b3f2ddf877be44a69f80e9f72fdd80c4c3544a
DefTruth <qiustudent_r@163.com>
2025-09-04 02:57:29 +0000

    allow empty blocks name

--- src/cache_dit/cache_factory/block_adapters/block_adapters.py
+++ src/cache_dit/cache_factory/block_adapters/block_adapters.py
@@ -457,10 +457,15 @@ class BlockAdapter:
             if BlockAdapter.nested_depth(attr) == 0:
                 normalized_attr = [[attr]]
             elif BlockAdapter.nested_depth(attr) == 1:
-                if len(attr) == len(adapter.transformer):
-                    normalized_attr = [[a] for a in attr]
-                else:
-                    normalized_attr = [attr]
+                if attr:
+                    if len(attr) == len(adapter.transformer):
+                        normalized_attr = [[a] for a in attr]
+                    else:
+                        normalized_attr = [attr]
+                else:  # []
+                    normalized_attr = [
+                        [] for _ in range(len(adapter.transformer))
+                    ]
 
             assert len(adapter.transformer) == len(normalized_attr)
             return normalized_attr
