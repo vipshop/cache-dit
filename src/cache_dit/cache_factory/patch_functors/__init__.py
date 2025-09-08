0336b23b9138a61d7774f4c9ca09669af55242e7
DefTruth <qiustudent_r@163.com>
2025-09-08 05:18:37 +0000

    feat: HunyuanDiT Patch Functor

--- src/cache_dit/cache_factory/patch_functors/__init__.py
+++ src/cache_dit/cache_factory/patch_functors/__init__.py
@@ -1,11 +1,11 @@
 from cache_dit.cache_factory.patch_functors.functor_base import PatchFunctor
 from cache_dit.cache_factory.patch_functors.functor_flux import FluxPatchFunctor
-from cache_dit.cache_factory.patch_functors.functor_hunyuan_dit import (
-    HunyuanDiTPatchFunctor,
-)
 from cache_dit.cache_factory.patch_functors.functor_chroma import (
     ChromaPatchFunctor,
 )
 from cache_dit.cache_factory.patch_functors.functor_hidream import (
     HiDreamPatchFunctor,
+)
+from cache_dit.cache_factory.patch_functors.functor_hunyuan_dit import (
+    HunyuanDiTPatchFunctor,
 )
