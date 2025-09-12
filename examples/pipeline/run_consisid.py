f2f7955313a654e645c20fbefa38a252931aff0c
DefTruth <qiustudent_r@163.com>
2025-09-12 06:08:42 +0000

    feat: add many examples

--- examples/pipeline/run_consisid.py
+++ examples/pipeline/run_consisid.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+sys.path.append("..")
+
+import time
 import torch
 from diffusers import ConsisIDPipeline
 from diffusers.pipelines.consisid.consisid_utils import (
@@ -5,12 +11,16 @@ from diffusers.pipelines.consisid.consisid_utils import (
     process_face_embeddings_infer,
 )
 from diffusers.utils import export_to_video
-from huggingface_hub import snapshot_download
+from utils import get_args, strify
+import cache_dit
 
-snapshot_download(
-    repo_id="BestWishYsh/ConsisID-preview",
-    local_dir="BestWishYsh/ConsisID-preview",
-)
+
+args = get_args()
+print(args)
+
+
+model_id = os.environ.get("CONSISID_DIR", "BestWishYsh/ConsisID-preview")
+
 (
     face_helper_1,
     face_helper_2,
@@ -18,17 +28,14 @@ snapshot_download(
     face_main_model,
     eva_transform_mean,
     eva_transform_std,
-) = prepare_face_models(
-    "BestWishYsh/ConsisID-preview", device="cuda", dtype=torch.bfloat16
-)
-pipe = ConsisIDPipeline.from_pretrained(
-    "BestWishYsh/ConsisID-preview", torch_dtype=torch.bfloat16
-)
+) = prepare_face_models(model_id, device="cuda", dtype=torch.bfloat16)
+pipe = ConsisIDPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16)
 pipe.to("cuda")
 
 # ConsisID works well with long and well-described prompts. Make sure the face in the image is clearly visible (e.g., preferably half-body or full-body).
 prompt = "The video captures a boy walking along a city street, filmed in black and white on a classic 35mm camera. His expression is thoughtful, his brow slightly furrowed as if he's lost in contemplation. The film grain adds a textured, timeless quality to the image, evoking a sense of nostalgia. Around him, the cityscape is filled with vintage buildings, cobblestone sidewalks, and softly blurred figures passing by, their outlines faint and indistinct. Streetlights cast a gentle glow, while shadows play across the boy's path, adding depth to the scene. The lighting highlights the boy's subtle smile, hinting at a fleeting moment of curiosity. The overall cinematic atmosphere, complete with classic film still aesthetics and dramatic contrasts, gives the scene an evocative and introspective feel."
-image = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/consisid/consisid_input.png?download=true"
+# image = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/consisid/consisid_input.png?download=true"
+image = "../data/consisid_input.png"
 
 id_cond, id_vit_hidden, image, face_kps = process_face_embeddings_infer(
     face_helper_1,
@@ -43,6 +50,21 @@ id_cond, id_vit_hidden, image, face_kps = process_face_embeddings_infer(
     is_align_face=True,
 )
 
+if args.cache:
+    cache_dit.enable_cache(
+        pipe,
+        Fn_compute_blocks=args.Fn,
+        Bn_compute_blocks=args.Bn,
+        max_warmup_steps=args.max_warmup_steps,
+        max_cached_steps=args.max_cached_steps,
+        max_continuous_cached_steps=args.max_continuous_cached_steps,
+        enable_taylorseer=args.taylorseer,
+        enable_encoder_taylorseer=args.taylorseer,
+        taylorseer_order=args.taylorseer_order,
+        residual_diff_threshold=args.rdt,
+    )
+
+start = time.time()
 video = pipe(
     image=image,
     prompt=prompt,
@@ -53,5 +75,14 @@ video = pipe(
     id_cond=id_cond,
     kps_cond=face_kps,
     generator=torch.Generator("cpu").manual_seed(42),
-)
-export_to_video(video.frames[0], "output.mp4", fps=8)
+).frames[0]
+
+end = time.time()
+
+cache_dit.summary(pipe, details=True)
+
+time_cost = end - start
+save_path = f"consisid.{strify(args, pipe)}.mp4"
+print(f"Time cost: {time_cost:.2f}s")
+print(f"Saving video to {save_path}")
+export_to_video(video, save_path, fps=8)
