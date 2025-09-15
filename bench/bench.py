9a447724f35ea1e0d650b906eabab86800b40d3f
DefTruth <qiustudent_r@163.com>
2025-09-15 03:59:16 +0000

    feat: add precision benchmark

--- bench/bench.py
+++ bench/bench.py
@@ -18,26 +18,27 @@ def get_args() -> argparse.ArgumentParser:
     parser.add_argument("--steps", type=int, default=50)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--cache", action="store_true", default=False)
-    parser.add_argument("--cache-config", type=str, default=None)
     parser.add_argument("--taylorseer", action="store_true", default=False)
     parser.add_argument("--taylorseer-order", "--order", type=int, default=1)
     parser.add_argument("--l1-diff", action="store_true", default=False)
     parser.add_argument("--rdt", type=float, default=0.08)
     parser.add_argument("--Fn-compute-blocks", "--Fn", type=int, default=1)
     parser.add_argument("--Bn-compute-blocks", "--Bn", type=int, default=0)
-    parser.add_argument("--max-warmup-steps", type=int, default=0)
-    parser.add_argument("--max-cached-steps", type=int, default=-1)
-    parser.add_argument("--max-continuous-cached-steps", type=int, default=-1)
+    parser.add_argument("--max-warmup-steps", "--w", type=int, default=0)
+    parser.add_argument("--max-cached-steps", "--mc", type=int, default=-1)
+    parser.add_argument(
+        "--max-continuous-cached-steps", "--mcc", type=int, default=-1
+    )
     parser.add_argument("--gen-device", type=str, default="cpu")
     parser.add_argument("--compile", action="store_true", default=False)
     parser.add_argument("--inductor-flags", action="store_true", default=False)
     parser.add_argument("--compile-all", action="store_true", default=False)
     parser.add_argument("--quantize", "-q", action="store_true", default=False)
     parser.add_argument(
-        "--use-block-adapter", "--adapt", action="store_true", default=False
-    )
-    parser.add_argument(
-        "--use-auto-block-adapter", "--auto", action="store_true", default=False
+        "--disable-block-adapter",
+        "--no-adapt",
+        action="store_true",
+        default=False,
     )
     parser.add_argument("--save-dir", type=str, default="./tmp/DrawBench200")
     parser.add_argument(
@@ -54,111 +55,65 @@ def set_rand_seeds(seed):
     torch.manual_seed(seed)
 
 
-def init_pipeline(args) -> FluxPipeline:
-    pipe = FluxPipeline.from_pretrained(
+def init_flux_pipe(args) -> FluxPipeline:
+    pipe: FluxPipeline = FluxPipeline.from_pretrained(
         os.environ.get("FLUX_DIR", "black-forest-labs/FLUX.1-dev"),
         torch_dtype=torch.bfloat16,
     ).to("cuda")
 
     # Apply cache to the pipeline
-    if args.cache or args.cache_config:
-        if not args.use_block_adapter:
-            if args.cache_config is None:
-                cache_dit.enable_cache(
-                    pipe,
-                    # Cache context kwargs
-                    Fn_compute_blocks=args.Fn_compute_blocks,
-                    Bn_compute_blocks=args.Bn_compute_blocks,
-                    max_warmup_steps=args.max_warmup_steps,
-                    max_cached_steps=args.max_cached_steps,
-                    max_continuous_cached_steps=args.max_continuous_cached_steps,
-                    residual_diff_threshold=args.rdt,
-                    l1_hidden_states_diff_threshold=(
-                        None if not args.l1_diff else args.rdt
-                    ),
-                    enable_taylorseer=args.taylorseer,
-                    enable_encoder_taylorseer=args.taylorseer,
-                    taylorseer_cache_type="residual",
-                    taylorseer_order=args.taylorseer_order,
-                )
-            else:
-                cache_dit.enable_cache(
-                    pipe, **cache_dit.load_options(args.cache_config)
-                )
-        else:
-            assert isinstance(pipe.transformer, FluxTransformer2DModel)
-            from cache_dit import ForwardPattern, BlockAdapter
-            from cache_dit.cache_factory.patch_functors import FluxPatchFunctor
+    if args.disable_block_adapter:
+        cache_dit.enable_cache(
+            pipe,
+            # Cache context kwargs
+            Fn_compute_blocks=args.Fn_compute_blocks,
+            Bn_compute_blocks=args.Bn_compute_blocks,
+            max_warmup_steps=args.max_warmup_steps,
+            max_cached_steps=args.max_cached_steps,
+            max_continuous_cached_steps=args.max_continuous_cached_steps,
+            residual_diff_threshold=args.rdt,
+            l1_hidden_states_diff_threshold=(
+                None if not args.l1_diff else args.rdt
+            ),
+            enable_taylorseer=args.taylorseer,
+            enable_encoder_taylorseer=args.taylorseer,
+            taylorseer_cache_type="residual",
+            taylorseer_order=args.taylorseer_order,
+        )
+    else:
+        assert isinstance(pipe.transformer, FluxTransformer2DModel)
+        from cache_dit import ForwardPattern, BlockAdapter
+        from cache_dit.cache_factory.patch_functors import FluxPatchFunctor
 
-            if args.cache_config is None:
-
-                cache_dit.enable_cache(
-                    # BlockAdapter & forward pattern
-                    (
-                        BlockAdapter(
-                            pipe=pipe,
-                            transformer=pipe.transformer,
-                            blocks=(
-                                pipe.transformer.transformer_blocks
-                                + pipe.transformer.single_transformer_blocks
-                            ),
-                            blocks_name="transformer_blocks",
-                            dummy_blocks_names=["single_transformer_blocks"],
-                            patch_functor=FluxPatchFunctor(),
-                            forward_pattern=ForwardPattern.Pattern_1,
-                        )
-                        if not args.use_auto_block_adapter
-                        else BlockAdapter(
-                            pipe=pipe,
-                            auto=True,
-                            blocks_policy="min",
-                            patch_functor=FluxPatchFunctor(),
-                            forward_pattern=ForwardPattern.Pattern_1,
-                        )
-                    ),
-                    # Cache context kwargs
-                    Fn_compute_blocks=args.Fn_compute_blocks,
-                    Bn_compute_blocks=args.Bn_compute_blocks,
-                    max_warmup_steps=args.max_warmup_steps,
-                    max_cached_steps=args.max_cached_steps,
-                    max_continuous_cached_steps=args.max_continuous_cached_steps,
-                    residual_diff_threshold=args.rdt,
-                    l1_hidden_states_diff_threshold=(
-                        None if not args.l1_diff else args.rdt
-                    ),
-                    enable_taylorseer=args.taylorseer,
-                    enable_encoder_taylorseer=args.taylorseer,
-                    taylorseer_cache_type="residual",
-                    taylorseer_order=args.taylorseer_order,
-                )
-            else:
-                cache_dit.enable_cache(
-                    # BlockAdapter & forward pattern
-                    (
-                        BlockAdapter(
-                            pipe,
-                            transformer=pipe.transformer,
-                            blocks=(
-                                pipe.transformer.transformer_blocks
-                                + pipe.transformer.single_transformer_blocks
-                            ),
-                            blocks_name="transformer_blocks",
-                            dummy_blocks_names=["single_transformer_blocks"],
-                            patch_functor=FluxPatchFunctor(),
-                            forward_pattern=ForwardPattern.Pattern_1,
-                        )
-                        if not args.use_auto_block_adapter
-                        else BlockAdapter(
-                            pipe=pipe,
-                            auto=True,
-                            blocks_policy="min",
-                            patch_functor=FluxPatchFunctor(),
-                            forward_pattern=ForwardPattern.Pattern_1,
-                        )
-                    ),
-                    # Cache context kwargs
-                    **cache_dit.load_options(args.cache_config),
-                )
+        cache_dit.enable_cache(
+            # BlockAdapter & forward pattern
+            BlockAdapter(
+                pipe=pipe,
+                transformer=pipe.transformer,
+                blocks=(
+                    pipe.transformer.transformer_blocks
+                    + pipe.transformer.single_transformer_blocks
+                ),
+                blocks_name="transformer_blocks",
+                dummy_blocks_names=["single_transformer_blocks"],
+                patch_functor=FluxPatchFunctor(),
+                forward_pattern=ForwardPattern.Pattern_1,
+            ),
+            # Cache context kwargs
+            Fn_compute_blocks=args.Fn_compute_blocks,
+            Bn_compute_blocks=args.Bn_compute_blocks,
+            max_warmup_steps=args.max_warmup_steps,
+            max_cached_steps=args.max_cached_steps,
+            max_continuous_cached_steps=args.max_continuous_cached_steps,
+            residual_diff_threshold=args.rdt,
+            l1_hidden_states_diff_threshold=(
+                None if not args.l1_diff else args.rdt
+            ),
+            enable_taylorseer=args.taylorseer,
+            enable_encoder_taylorseer=args.taylorseer,
+            taylorseer_cache_type="residual",
+            taylorseer_order=args.taylorseer_order,
+        )
 
     if args.quantize:
         # Apply Quantization (default: FP8 DQ) to Transformer
@@ -173,28 +128,18 @@ def init_pipeline(args) -> FluxPipeline:
             torch._dynamo.config.accumulated_recompile_limit = (
                 2048  # default is 256
             )
-        if isinstance(pipe.transformer, FluxTransformer2DModel):
-            if not args.compile_all:
-                logger.warning(
-                    "Only compile transformer blocks not the whole model "
-                    "for FluxTransformer2DModel to keep higher precision."
-                )
-                for module in pipe.transformer.transformer_blocks:
-                    module.compile(fullgraph=True)
-                for module in pipe.transformer.single_transformer_blocks:
-                    module.compile(fullgraph=True)
-            else:
-                pipe.transformer = torch.compile(
-                    pipe.transformer, mode="default"
-                )
+        if not args.compile_all:
+            logger.warning(
+                "Only compile transformer blocks not the whole model "
+                "for FluxTransformer2DModel to keep higher precision."
+            )
+            pipe.transformer.compile_repeated_blocks(fullgraph=True)
         else:
-            logger.info("Compiling the transformer with default mode.")
             pipe.transformer = torch.compile(pipe.transformer, mode="default")
-
     return pipe
 
 
-def gen_image(args, pipe: FluxPipeline, prompt) -> Image.Image:
+def gen_flux_image(args, pipe: FluxPipeline, prompt) -> Image.Image:
     assert prompt is not None
     image = pipe(
         prompt,
@@ -206,11 +151,12 @@ def gen_image(args, pipe: FluxPipeline, prompt) -> Image.Image:
 
 @torch.no_grad()
 def main():
+    # TODO: Support more pipelines, such as Qwen-Image, DiT-XL, etc.
     args = get_args()
     logger.info(f"Arguments: {args}")
     set_rand_seeds(args.seed)
 
-    pipe = init_pipeline(args)
+    pipe = init_flux_pipe(args)
     pipe.set_progress_bar_config(disable=True)
 
     # Load prompts
@@ -230,7 +176,7 @@ def main():
 
     for i, prompt in tqdm(enumerate(prompts), total=len(prompts)):
         start = time.time()
-        image = gen_image(args, pipe, prompt=prompt)
+        image = gen_flux_image(args, pipe, prompt=prompt)
         end = time.time()
         all_times.append(end - start)
         save_name = os.path.join(save_dir, f"img_{i}.png")
@@ -243,3 +189,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 8
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 4
+    # python3 bench.py --cache --Fn 4 --Bn 0 --max-warmup-steps 4
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 8 --mcc 3 --rdt 0.16
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 8 --mcc 2 --rdt 0.16
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 4 --mcc 3 --rdt 0.16
+    # python3 bench.py --cache --Fn 8 --Bn 0 --max-warmup-steps 4 --mcc 2 --rdt 0.16
+    # python3 bench.py --cache --Fn 4 --Bn 0 --max-warmup-steps 4 --mcc 2 --rdt 0.16
