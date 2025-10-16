52f7896164b1ce6ee2d3569c96c8cb48188661e2
DefTruth <qiustudent_r@163.com>
2025-10-16 11:03:51 +0000

    feat: add wan2.2 i2v example

--- examples/pipeline/run_wan_2.2_i2v.py
+++ examples/pipeline/run_wan_2.2_i2v.py
@@ -113,7 +113,6 @@ image = load_image(
 )
 max_area = 480 * 832
 aspect_ratio = image.height / image.width
-print(pipe.vae_scale_factor_spatial, pipe.vae_scale_factor_temporal)
 mod_value = (
     pipe.vae_scale_factor_spatial * pipe.transformer.config.patch_size[1]
 )
@@ -123,8 +122,6 @@ image = image.resize((width, height))
 
 prompt = "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside."
 negative_prompt = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
-
-print(f"Input image resized to {width}x{height}")
 
 
 def run_pipe():
