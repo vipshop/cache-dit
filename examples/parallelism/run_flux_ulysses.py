67a3b4361c13bf443bc1f9ce5b595458ecc45ddc
DefTruth <qiustudent_r@163.com>
2025-10-17 08:45:48 +0000

    feat: support diffusers' native context parallelism

--- examples/parallelism/run_flux_ulysses.py
+++ examples/parallelism/run_flux_ulysses.py
@@ -15,13 +15,21 @@ from utils import get_args, strify, cachify
 import cache_dit
 
 
-args = get_args()
+parser = get_args(parse=False)
+parser.add_argument(
+    "--parallel-type",
+    type=str,
+    default="none",
+    choices=["ulysses", "ring", "none"],
+)
+args = parser.parse_args()
 print(args)
 
-dist.init_process_group("nccl")
-rank = dist.get_rank()
-device = torch.device("cuda", rank % torch.cuda.device_count())
-torch.cuda.set_device(device)
+if args.parallel_type != "none":
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    device = torch.device("cuda", rank % torch.cuda.device_count())
+    torch.cuda.set_device(device)
 
 
 pipe: FluxPipeline = FluxPipeline.from_pretrained(
@@ -37,9 +45,26 @@ if args.cache:
 
 assert isinstance(pipe.transformer, FluxTransformer2DModel)
 
-pipe.transformer.enable_parallelism(
-    config=ContextParallelConfig(ulysses_degree=dist.get_world_size()),
-)
+if args.parallel_type != "none":
+    # Now only _native_cudnn is supported for parallelism
+    # issue: https://github.com/huggingface/diffusers/pull/12443
+    pipe.transformer.set_attention_backend("_native_cudnn")
+
+if args.parallel_type == "ulysses":
+    pipe.transformer.enable_parallelism(
+        config=ContextParallelConfig(ulysses_degree=dist.get_world_size()),
+    )
+elif args.parallel_type == "ring":
+    pipe.transformer.enable_parallelism(
+        config=ContextParallelConfig(ring_degree=dist.get_world_size()),
+    )
+else:
+    print("No parallelism is enabled.")
+
+if args.compile:
+    cache_dit.set_compile_configs()
+    pipe.transformer = torch.compile(pipe.transformer)
+
 
 start = time.time()
 image = pipe(
@@ -50,11 +75,20 @@ image = pipe(
 
 end = time.time()
 
-if rank == 0:
+if args.parallel_type != "none":
+    if rank == 0:
+        cache_dit.summary(pipe)
+
+        time_cost = end - start
+        save_path = f"flux.{args.parallel_type}{dist.get_world_size()}.{strify(args, pipe)}.png"
+        print(f"Time cost: {time_cost:.2f}s")
+        print(f"Saving image to {save_path}")
+        image.save(save_path)
+else:
     cache_dit.summary(pipe)
 
     time_cost = end - start
-    save_path = f"flux.ulysses{dist.get_world_size()}.{strify(args, pipe)}.png"
+    save_path = f"flux.{strify(args, pipe)}.png"
     print(f"Time cost: {time_cost:.2f}s")
     print(f"Saving image to {save_path}")
     image.save(save_path)
