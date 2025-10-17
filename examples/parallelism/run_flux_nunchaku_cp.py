85db99376b0dcd416daaa049f9619f561da00c3c
DefTruth <qiustudent_r@163.com>
2025-10-17 10:25:39 +0000

    feat: support diffusers' native context parallelism

--- examples/parallelism/run_flux_nunchaku_cp.py
+++ examples/parallelism/run_flux_nunchaku_cp.py
@@ -96,6 +96,7 @@ if args.parallel_type != "none":
 
 # Patch: https://github.com/nunchaku-tech/nunchaku/blob/main/nunchaku/models/attention_processors/flux.py#L87
 # to support distributed context parallelism for Flux in nunchaku please add below code:
+# query, key, value = qkv.chunk(3, dim=-1)
 # if (
 #     torch.distributed.is_initialized()
 #     and torch.distributed.get_world_size() > 1
@@ -104,12 +105,25 @@ if args.parallel_type != "none":
 
 #     seq_len = key.shape[1]
 #     if seq_len > 100:
-#         key_list = [torch.empty_like(key) for _ in range(world_size)]
-#         value_list = [torch.empty_like(value) for _ in range(world_size)]
-#         torch.distributed.all_gather(key_list, key.contiguous())
-#         torch.distributed.all_gather(value_list, value.contiguous())
-#         key = torch.cat(key_list, dim=1)
-#         value = torch.cat(value_list, dim=1)
+#         key_chunk_size = key.size(1) // world_size
+#         value_chunk_size = value.size(1) // world_size
+
+#         key_send = list(torch.split(key.contiguous(), key_chunk_size, dim=1))
+#         value_send = list(
+#             torch.split(value.contiguous(), value_chunk_size, dim=1)
+#         )
+
+#         key_recv = [torch.empty_like(key_send[0]) for _ in range(world_size)]
+#         value_recv = [
+#             torch.empty_like(value_send[0]) for _ in range(world_size)
+#         ]
+
+#         torch.distributed.all_to_all(key_recv, key_send)
+#         torch.distributed.all_to_all(value_recv, value_send)
+
+#         key = torch.cat(key_recv, dim=1)
+#         value = torch.cat(value_recv, dim=1)
+
 
 if args.parallel_type == "ulysses":
     pipe.transformer.enable_parallelism(
@@ -126,7 +140,7 @@ else:
 def run_pipe(pipe: FluxPipeline):
     image = pipe(
         "A cat holding a sign that says hello world",
-        num_inference_steps=10,
+        num_inference_steps=28,
         height=960,
         width=1440 + 480,
         generator=torch.Generator("cpu").manual_seed(0),
