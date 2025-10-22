780b758745590231ea78c6d94cfaa8cc9bbda7b3
DefTruth <qiustudent_r@163.com>
2025-10-22 05:50:35 +0000

    feat: seperate requirements

--- setup.py
+++ setup.py
@@ -36,21 +36,6 @@ def my_local_scheme(version):
     return f"+{local_version}"
 
 
-def fetch_core_requirements():
-    with open("requirements/core.txt") as f:
-        return f.read().strip().split("\n")
-
-
-def fetch_quantization_requirements():
-    with open("requirements/quantization.txt") as f:
-        return f.read().strip().split("\n")
-
-
-def fetch_metrics_requirements():
-    with open("requirements/metrics.txt") as f:
-        return f.read().strip().split("\n")
-
-
 setup(
     name=PACKAGE_NAME,
     description="A Unified, Flexible and Training-free Cache Acceleration Framework for 🤗Diffusers.",
@@ -66,35 +51,5 @@ setup(
         "console_scripts": [
             "cache-dit-metrics-cli = cache_dit.metrics:main",
         ],
-    },
-    # pip3 install cache-dit, installs core and quantization requirements by default
-    install_requires=fetch_core_requirements()
-    + fetch_quantization_requirements(),
-    extras_require={
-        # pip3 install 'cache-dit[core]'
-        "core": fetch_core_requirements(),
-        # pip3 install 'cache-dit[quantization]'
-        "quantization": fetch_quantization_requirements(),
-        # pip3 install 'cache-dit[metrics]
-        "metrics": fetch_metrics_requirements(),
-        # pip3 install 'cache-dit[dev]'
-        "dev": [
-            "pre-commit",
-            "pytest>=7.0.0,<8.0.0",
-            "pytest-html",
-            "expecttest",
-            "hypothesis",
-            "accelerate",
-            "peft",
-            "protobuf",
-            "sentencepiece",
-            "opencv-python-headless",
-            "ftfy",
-            "scikit-image",
-        ],
-        # pip3 install 'cache-dit[all]'
-        "all": fetch_core_requirements()
-        + fetch_quantization_requirements()
-        + fetch_metrics_requirements(),
     },
 )
