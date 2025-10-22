24d3b17a437b0132c4bdce4692b099838d6f6a95
DefTruth <qiustudent_r@163.com>
2025-10-22 05:05:46 +0000

    feat: seperate requirements

--- setup.py
+++ setup.py
@@ -93,8 +93,6 @@ setup(
             "scikit-image",
         ],
         # pip3 install 'cache-dit[all]'
-        "all": fetch_core_requirements()
-        + fetch_quantization_requirements()
-        + fetch_metrics_requirements(),
+        "all": fetch_metrics_requirements(),
     },
 )
