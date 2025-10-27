98b20c82398c54089e78a0706c2eb7a4d3b027c1
DefTruth <qiustudent_r@163.com>
2025-10-27 06:21:38 +0000

    feat: support transformer-only api

--- examples/api/run_transformer_only.py
+++ examples/api/run_transformer_only.py
@@ -5,7 +5,7 @@ sys.path.append("..")
 
 import time
 import torch
-from diffusers import FluxPipeline
+from diffusers import FluxPipeline, FluxTransformer2DModel
 from utils import get_args, strify, cachify
 import cache_dit
 
@@ -23,21 +23,47 @@ pipe = FluxPipeline.from_pretrained(
 ).to("cuda")
 
 if args.cache:
-    cachify(args, pipe)
+    from cache_dit import BlockAdapter, ForwardPattern
+
+    assert isinstance(pipe.transformer, FluxTransformer2DModel)
+
+    cachify(
+        args,
+        BlockAdapter(
+            transformer=pipe.transformer,
+            blocks=[
+                pipe.transformer.transformer_blocks,
+                pipe.transformer.single_transformer_blocks,
+            ],
+            forward_pattern=[
+                ForwardPattern.Pattern_1,
+                ForwardPattern.Pattern_1,
+            ],
+            check_forward_pattern=True,
+        ),
+    )
+
+
+def run_pipe():
+    image = pipe(
+        "A cat holding a sign that says hello world",
+        num_inference_steps=28,
+        generator=torch.Generator("cpu").manual_seed(0),
+    ).images[0]
+    return image
+
+
+# warmup
+_ = run_pipe()
 
 start = time.time()
-image = pipe(
-    "A cat holding a sign that says hello world",
-    num_inference_steps=28,
-    generator=torch.Generator("cpu").manual_seed(0),
-).images[0]
-
+image = run_pipe()
 end = time.time()
 
-cache_dit.summary(pipe)
+cache_dit.summary(pipe.transformer)
 
 time_cost = end - start
-save_path = f"flux.{strify(args, pipe)}.png"
+save_path = f"flux.{strify(args, pipe.transformer)}.png"
 print(f"Time cost: {time_cost:.2f}s")
 print(f"Saving image to {save_path}")
 image.save(save_path)
