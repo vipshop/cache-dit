75cd066311e8879c4e47deeaa423a682b622988d
G.O.D <32255912+gameofdimension@users.noreply.github.com>
2025-10-28 19:04:48 +0800

    feat: support Qwen-Image tensor parallelism (#331)
    * support Qwen-Image tp
    
    * update
    
    * clean code
    
    ---------
    
    Co-authored-by: felix01.yu <felix01.yu@vipshop.com>

--- src/cache_dit/parallelism/backends/native_pytorch/parallel_torch.py
+++ src/cache_dit/parallelism/backends/native_pytorch/parallel_torch.py
@@ -34,21 +34,27 @@ def maybe_enable_parallelism(
     ):
         from torch.distributed import DeviceMesh, init_device_mesh
 
-        from cache_dit.parallelism.backends.native_pytorch.tensor_parallelism.flux.parallelize import (
-            dit_apply_tp,
-        )
-
         tp_mesh: DeviceMesh = init_device_mesh(
             device_type="cuda",
             mesh_shape=[parallelism_config.tp_size],
         )
 
-        assert transformer.__class__.__name__.startswith(
-            "Flux"
-        ), "tensor parallelism currently only supports Flux models."
+        class_name = transformer.__class__.__name__
+        if class_name.startswith("Flux"):
+            from cache_dit.parallelism.backends.native_pytorch.tensor_parallelism.flux.parallelize import (
+                dit_apply_tp,
+            )
+        elif class_name.startswith("QwenImage"):
+            from cache_dit.parallelism.backends.native_pytorch.tensor_parallelism.qwen_image.parallelize import (
+                dit_apply_tp,
+            )
+        else:
+            raise NotImplementedError(
+                f"TP for {class_name} is not implemented yet."
+            )
+
         transformer = dit_apply_tp(
             transformer,
             tp_mesh=tp_mesh,
         )
-
     return transformer
