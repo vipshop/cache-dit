3299da85898be797c95b58394c5a09b21ac6ba0d
felix01.yu <felix01.yu@vipshop.com>
2025-10-30 14:54:45 +0800

    fix import

--- src/cache_dit/parallelism/backends/native_pytorch/tensor_parallelism/tp_planners.py
+++ src/cache_dit/parallelism/backends/native_pytorch/tensor_parallelism/tp_planners.py
@@ -2,9 +2,11 @@
 from .tp_plan_registers import TensorParallelismPlannerRegister
 from .tp_plan_flux import FluxTensorParallelismPlanner
 from .tp_plan_qwen_image import QwenImageTensorParallelismPlanner
+from .tp_plan_wan import WanTensorParallelismPlanner
 
 __all__ = [
     "TensorParallelismPlannerRegister",
     "FluxTensorParallelismPlanner",
     "QwenImageTensorParallelismPlanner",
+    "WanTensorParallelismPlanner",
 ]
