deafe22b98ad73a53e6e0fe5f9d41e482f1a04ac
G.O.D <32255912+gameofdimension@users.noreply.github.com>
2025-11-03 19:59:27 +0800

    feat: support TP for hunyuanimage2.1/video (#373)
    * test hyimage
    
    * add hunyuan video example
    
    * avoid oom
    
    * fix qwimage
    
    * fix tp diff
    
    * restore file
    
    * rename case
    
    * rename case
    
    * update deps
    
    * resume deps and add notice
    
    ---------
    
    Co-authored-by: felix01.yu <felix01.yu@vipshop.com>

--- src/cache_dit/parallelism/backends/native_pytorch/tensor_parallelism/tp_plan_flux.py
+++ src/cache_dit/parallelism/backends/native_pytorch/tensor_parallelism/tp_plan_flux.py
@@ -22,6 +22,8 @@ from cache_dit.logger import init_logger
 logger = init_logger(__name__)
 
 
+@TensorParallelismPlannerRegister.register("HunyuanImage")
+@TensorParallelismPlannerRegister.register("HunyuanVideo")
 @TensorParallelismPlannerRegister.register("Flux")
 class FluxTensorParallelismPlanner(TensorParallelismPlanner):
     def apply(
@@ -120,7 +122,7 @@ class FluxTensorParallelismPlanner(TensorParallelismPlanner):
             single_block: FluxSingleTransformerBlock, tp_group_size
         ):
             # rowwise
-            hidden_dim = 3072
+            hidden_dim = single_block.attn.to_q.weight.shape[0]
             requires_grad = single_block.proj_out.weight.requires_grad
             linear2_weight_data = (
                 single_block.proj_out.weight.data.T.detach().clone()
