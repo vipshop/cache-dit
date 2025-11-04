e06638c40b3cde28b119e65053b3ac9889781506
DefTruth <qiustudent_r@163.com>
2025-11-04 09:27:50 +0000

    feat: support hyvideo context parallel

---
+++ examples/parallelism/run_hunyuan_video_cp.py
@@ -0,0 +1,115 @@
+import os
+import sys
+
+sys.path.append("..")
+
+import time
+
+import torch
+from diffusers import (
+    HunyuanVideoPipeline,
+    HunyuanVideoTransformer3DModel,
+    AutoencoderKLHunyuanVideo,
+)
+from diffusers.quantizers import PipelineQuantizationConfig
+from diffusers.utils import export_to_video
+from utils import (
+    GiB,
+    cachify,
+    get_args,
+    maybe_destroy_distributed,
+    maybe_init_distributed,
+    strify,
+)
+
+import cache_dit
+
+args = get_args()
+print(args)
+
+rank, device = maybe_init_distributed(args)
+
+enable_quatization = args.quantize and GiB() < 96
+
+pipe: HunyuanVideoPipeline = HunyuanVideoPipeline.from_pretrained(
+    os.environ.get(
+        "HUNYUAN_VIDEO_DIR",
+        "hunyuanvideo-community/HunyuanVideo",
+    ),
+    torch_dtype=torch.bfloat16,
+    quantization_config=(
+        PipelineQuantizationConfig(
+            quant_backend="bitsandbytes_4bit",
+            quant_kwargs={
+                "load_in_4bit": True,
+                "bnb_4bit_quant_type": "nf4",
+                "bnb_4bit_compute_dtype": torch.bfloat16,
+            },
+            components_to_quantize=["text_encoder"],  # 4GiB
+        )
+        if enable_quatization
+        else None
+    ),
+)
+
+
+if GiB() < 96:
+    if enable_quatization:
+        pipe.transformer = cache_dit.quantize(
+            pipe.transformer,
+            quant_type=args.quantize_type,  # float8_weight_only
+        )
+        pipe.to(device)
+else:
+    pipe.to(device)
+
+
+if args.cache or args.parallel_type is not None:
+    cachify(args, pipe)
+
+assert isinstance(pipe.transformer, HunyuanVideoTransformer3DModel)
+
+if GiB() < 96 and not enable_quatization:
+    pipe.enable_model_cpu_offload(device=device)
+
+assert isinstance(pipe.vae, AutoencoderKLHunyuanVideo)
+pipe.vae.enable_tiling()
+
+pipe.set_progress_bar_config(disable=rank != 0)
+
+
+def run_pipe(warmup: bool = False):
+    prompt = "A cat walks on the grass, realistic"
+    output = pipe(
+        prompt,
+        height=320,
+        width=512,
+        num_frames=61,
+        num_inference_steps=30 if not warmup else 5,
+        generator=torch.Generator("cpu").manual_seed(0),
+    ).frames[0]
+    return output
+
+
+if args.compile:
+    cache_dit.set_compile_configs()
+    pipe.transformer = torch.compile(pipe.transformer)
+
+# warmup
+_ = run_pipe(warmup=True)
+
+start = time.time()
+video = run_pipe()
+end = time.time()
+
+if rank == 0:
+    cache_dit.summary(pipe)
+
+    time_cost = end - start
+    save_path = f"hunyuan_video.{strify(args, pipe)}.mp4"
+    print(f"Time cost: {time_cost:.2f}s")
+    print(f"Saving video to {save_path}")
+    export_to_video(video, save_path, fps=15)
+
+
+maybe_destroy_distributed()
--- src/cache_dit/parallelism/backends/native_diffusers/context_parallelism/cp_plan_hunyuan.py
+++ src/cache_dit/parallelism/backends/native_diffusers/context_parallelism/cp_plan_hunyuan.py
@@ -1,6 +1,6 @@
 import torch
 import functools
-from typing import Optional, Dict, Any, Union
+from typing import Optional, Dict, Any, Union, Tuple
 from diffusers.models.modeling_utils import ModelMixin
 from diffusers.utils import (
     USE_PEFT_BACKEND,
@@ -8,6 +8,7 @@ from diffusers.utils import (
     unscale_lora_layers,
 )
 from diffusers.models.modeling_outputs import Transformer2DModelOutput
+from diffusers import HunyuanVideoTransformer3DModel
 
 try:
     from diffusers import HunyuanImageTransformer2DModel
@@ -331,6 +332,265 @@ def __patch__HunyuanImageTransformer2DModel_forward__(
         for post_patch, patch in zip(post_patch_sizes, self.config.patch_size)
     ]
     hidden_states = hidden_states.reshape(*final_dims)
+
+    if USE_PEFT_BACKEND:
+        # remove `lora_scale` from each PEFT layer
+        unscale_lora_layers(self, lora_scale)
+
+    if not return_dict:
+        return (hidden_states,)
+
+    return Transformer2DModelOutput(sample=hidden_states)
+
+
+@ContextParallelismPlannerRegister.register("HunyuanVideo")
+class HunyuanVideoContextParallelismPlanner(ContextParallelismPlanner):
+    def apply(
+        self,
+        transformer: Optional[torch.nn.Module | ModelMixin] = None,
+        **kwargs,
+    ) -> ContextParallelModelPlan:
+
+        # NOTE: Diffusers native CP plan still not supported
+        # for HunyuanImage now.
+        self._cp_planner_preferred_native_diffusers = False
+
+        if (
+            transformer is not None
+            and self._cp_planner_preferred_native_diffusers
+        ):
+            assert isinstance(
+                transformer, HunyuanVideoTransformer3DModel
+            ), "Transformer must be an instance of HunyuanVideoTransformer3DModel"
+            if hasattr(transformer, "_cp_plan"):
+                if transformer._cp_plan is not None:
+                    return transformer._cp_plan
+
+        # Apply monkey patch to fix attention mask preparation while using CP
+        assert isinstance(transformer, HunyuanVideoTransformer3DModel)
+        HunyuanVideoTransformer3DModel.forward = (
+            __patch__HunyuanVideoTransformer3DModel_forward__
+        )
+
+        # Otherwise, use the custom CP plan defined here, this maybe
+        # a little different from the native diffusers implementation
+        # for some models.
+        _cp_plan = {
+            # Pattern of rope, split_output=True (split output rather than input):
+            #    un-split input
+            #    -> keep input un-split
+            #    -> rope
+            #    -> splited output
+            "rope": {
+                0: ContextParallelInput(
+                    split_dim=0, expected_dims=2, split_output=True
+                ),
+                1: ContextParallelInput(
+                    split_dim=0, expected_dims=2, split_output=True
+                ),
+            },
+            # Pattern of transformer_blocks.0, split_output=False:
+            #     un-split input -> split -> to_qkv/...
+            #     -> all2all
+            #     -> attn (local head, full seqlen)
+            #     -> all2all
+            #     -> splited output
+            # Pattern of the rest transformer_blocks, single_transformer_blocks:
+            #     splited input (previous splited output) -> to_qkv/...
+            #     -> all2all
+            #     -> attn (local head, full seqlen)
+            #     -> all2all
+            #     -> splited output
+            # The `encoder_hidden_states` will be changed after each block forward,
+            # so we need to split it at the first block, and keep it splited (namely,
+            # automatically split by the all2all op after attn) for the rest blocks.
+            # The `out` tensor of local attn will be splited into `hidden_states` and
+            # `encoder_hidden_states` after each block forward, thus both of them
+            # will be automatically splited by all2all comm op after local attn.
+            "transformer_blocks.0": {
+                "hidden_states": ContextParallelInput(
+                    split_dim=1, expected_dims=3, split_output=False
+                ),
+                "encoder_hidden_states": ContextParallelInput(
+                    split_dim=1, expected_dims=3, split_output=False
+                ),
+            },
+            # NOTE: We have to handle the `attention_mask` carefully in monkey-patched
+            # transformer forward while using CP, since it is not splited here.
+            # Then, the final proj_out will gather the splited output.
+            #     splited input (previous splited output)
+            #     -> all gather
+            #     -> un-split output
+            "proj_out": ContextParallelOutput(gather_dim=1, expected_dims=3),
+        }
+        return _cp_plan
+
+
+@functools.wraps(HunyuanVideoTransformer3DModel.forward)
+def __patch__HunyuanVideoTransformer3DModel_forward__(
+    self,
+    hidden_states: torch.Tensor,
+    timestep: torch.LongTensor,
+    encoder_hidden_states: torch.Tensor,
+    encoder_attention_mask: torch.Tensor,
+    pooled_projections: torch.Tensor,
+    guidance: torch.Tensor = None,
+    attention_kwargs: Optional[Dict[str, Any]] = None,
+    return_dict: bool = True,
+) -> Union[Tuple[torch.Tensor], Transformer2DModelOutput]:
+    if attention_kwargs is not None:
+        attention_kwargs = attention_kwargs.copy()
+        lora_scale = attention_kwargs.pop("scale", 1.0)
+    else:
+        lora_scale = 1.0
+
+    if USE_PEFT_BACKEND:
+        # weight the lora layers by setting `lora_scale` for each PEFT layer
+        scale_lora_layers(self, lora_scale)
+    else:
+        if (
+            attention_kwargs is not None
+            and attention_kwargs.get("scale", None) is not None
+        ):
+            logger.warning(
+                "Passing `scale` via `attention_kwargs` when not using the PEFT backend is ineffective."
+            )
+
+    batch_size, num_channels, num_frames, height, width = hidden_states.shape
+    p, p_t = self.config.patch_size, self.config.patch_size_t
+    post_patch_num_frames = num_frames // p_t
+    post_patch_height = height // p
+    post_patch_width = width // p
+    first_frame_num_tokens = 1 * post_patch_height * post_patch_width
+
+    # 1. RoPE
+    image_rotary_emb = self.rope(hidden_states)
+
+    # 2. Conditional embeddings
+    temb, token_replace_emb = self.time_text_embed(
+        timestep, pooled_projections, guidance
+    )
+
+    hidden_states = self.x_embedder(hidden_states)
+    encoder_hidden_states = self.context_embedder(
+        encoder_hidden_states, timestep, encoder_attention_mask
+    )
+
+    # 3. Attention mask preparation
+    latent_sequence_length = hidden_states.shape[1]
+    condition_sequence_length = encoder_hidden_states.shape[1]
+    sequence_length = latent_sequence_length + condition_sequence_length
+    attention_mask = torch.ones(
+        batch_size,
+        sequence_length,
+        device=hidden_states.device,
+        dtype=torch.bool,
+    )  # [B, N]
+    effective_condition_sequence_length = encoder_attention_mask.sum(
+        dim=1, dtype=torch.int
+    )  # [B,]
+    effective_sequence_length = (
+        latent_sequence_length + effective_condition_sequence_length
+    )
+    indices = torch.arange(
+        sequence_length, device=hidden_states.device
+    ).unsqueeze(
+        0
+    )  # [1, N]
+    mask_indices = indices >= effective_sequence_length.unsqueeze(1)  # [B, N]
+    attention_mask = attention_mask.masked_fill(mask_indices, False)
+    # NOTE(DefTruth): Permute attention_mask if context parallel is used.
+    # For example, if work size = 2: [H, E] -> [H_0, E_0, H_1, E_1]
+    if self._parallel_config is not None:
+        cp_config = getattr(
+            self._parallel_config, "context_parallel_config", None
+        )
+        if cp_config is not None and cp_config._world_size > 1:
+            hidden_mask = attention_mask[:, :latent_sequence_length]
+            encoder_mask = attention_mask[:, latent_sequence_length:]
+            hidden_mask_splits = torch.chunk(
+                hidden_mask, cp_config._world_size, dim=1
+            )
+            encoder_mask_splits = torch.chunk(
+                encoder_mask, cp_config._world_size, dim=1
+            )
+            new_attention_mask_splits = []
+            for i in range(cp_config._world_size):
+                new_attention_mask_splits.append(hidden_mask_splits[i])
+                new_attention_mask_splits.append(encoder_mask_splits[i])
+            attention_mask = torch.cat(new_attention_mask_splits, dim=1)
+
+    attention_mask = attention_mask.unsqueeze(1).unsqueeze(1)  # [B, 1, 1, N]
+
+    # 4. Transformer blocks
+    if torch.is_grad_enabled() and self.gradient_checkpointing:
+        for block in self.transformer_blocks:
+            hidden_states, encoder_hidden_states = (
+                self._gradient_checkpointing_func(
+                    block,
+                    hidden_states,
+                    encoder_hidden_states,
+                    temb,
+                    attention_mask,
+                    image_rotary_emb,
+                    token_replace_emb,
+                    first_frame_num_tokens,
+                )
+            )
+
+        for block in self.single_transformer_blocks:
+            hidden_states, encoder_hidden_states = (
+                self._gradient_checkpointing_func(
+                    block,
+                    hidden_states,
+                    encoder_hidden_states,
+                    temb,
+                    attention_mask,
+                    image_rotary_emb,
+                    token_replace_emb,
+                    first_frame_num_tokens,
+                )
+            )
+
+    else:
+        for block in self.transformer_blocks:
+            hidden_states, encoder_hidden_states = block(
+                hidden_states,
+                encoder_hidden_states,
+                temb,
+                attention_mask,
+                image_rotary_emb,
+                token_replace_emb,
+                first_frame_num_tokens,
+            )
+
+        for block in self.single_transformer_blocks:
+            hidden_states, encoder_hidden_states = block(
+                hidden_states,
+                encoder_hidden_states,
+                temb,
+                attention_mask,
+                image_rotary_emb,
+                token_replace_emb,
+                first_frame_num_tokens,
+            )
+
+    # 5. Output projection
+    hidden_states = self.norm_out(hidden_states, temb)
+    hidden_states = self.proj_out(hidden_states)
+
+    hidden_states = hidden_states.reshape(
+        batch_size,
+        post_patch_num_frames,
+        post_patch_height,
+        post_patch_width,
+        -1,
+        p_t,
+        p,
+        p,
+    )
+    hidden_states = hidden_states.permute(0, 4, 1, 5, 2, 6, 3, 7)
+    hidden_states = hidden_states.flatten(6, 7).flatten(4, 5).flatten(2, 3)
 
     if USE_PEFT_BACKEND:
         # remove `lora_scale` from each PEFT layer
