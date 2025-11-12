14bbf502340669e30da44c2ad8782634c2dd3f63
felix01.yu <felix01.yu@vipshop.com>
2025-11-12 15:55:04 +0800

    update

--- examples/parallelism/run_cogview4_tp.py
+++ examples/parallelism/run_cogview4_tp.py
@@ -25,10 +25,10 @@ pipe = CogView4Pipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
 )
 
-pipe.to("cuda")
-
 if args.cache or args.parallel_type is not None:
     cachify(args, pipe, enable_separate_cfg=True)
+torch.cuda.empty_cache()
+pipe.enable_model_cpu_offload(device=device)
 
 prompt = "A vibrant cherry red sports car sits proudly under the gleaming sun, its polished exterior smooth and flawless, casting a mirror-like reflection. The car features a low, aerodynamic body, angular headlights that gaze forward like predatory eyes, and a set of black, high-gloss racing rims that contrast starkly with the red. A subtle hint of chrome embellishes the grille and exhaust, while the tinted windows suggest a luxurious and private interior. The scene conveys a sense of speed and elegance, the car appearing as if it's about to burst into a sprint along a coastal road, with the ocean's azure waves crashing in the background."
 
