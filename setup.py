44018778f43d3235a93412f4b8e512c8f66154da
DefTruth <qiustudent_r@163.com>
2025-11-18 02:29:33 +0000

    Bump up to v1.1.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def my_local_scheme(version):
 
 setup(
     name=PACKAGE_NAME,
-    description="A Unified and Flexible Inference Engine with Hybrid Cache Acceleration and Parallelism for 🤗Diffusers.",
+    description="A Unified and Flexible Inference Engine with Hybrid Cache Acceleration and Parallelism for 🤗DiTs.",
     author="DefTruth, vipshop.com",
     use_scm_version={
         "write_to": path.join("src", "cache_dit", "_version.py"),
