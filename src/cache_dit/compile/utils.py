9936b5c901568d86be71f0f5bcefc67218388a87
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-11-24 17:25:20 +0800

    feat: allow UAA in compiled graph (#474)
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph
    
    * feat: allow UAA in compiled graph

--- src/cache_dit/compile/utils.py
+++ src/cache_dit/compile/utils.py
@@ -23,11 +23,38 @@ def epilogue_prologue_fusion_enabled(**kwargs) -> bool:
     return CACHE_DIT_EPILOGUE_PROLOGUE_FUSION or mode
 
 
+_CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP = (
+    os.environ.get(
+        "CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP",
+        "1",
+    )
+    == "1"
+)
+
+
+def enable_compile_compute_comm_overlap():
+    global _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP
+    _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP = True
+    logger.info("Enabled compile compute-communication overlap manually.")
+
+
+def disable_compile_compute_comm_overlap():
+    global _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP
+    _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP = False
+    logger.info("Disabled compile compute-communication overlap manually.")
+
+
+def is_compile_compute_comm_overlap_enabled() -> bool:
+    global _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP
+    return _CACHE_DIT_ENABLE_COMPILE_COMPUTE_COMM_OVERLAP
+
+
 def set_compile_configs(
     descent_tuning: bool = False,
     cuda_graphs: bool = False,
     force_disable_compile_caches: bool = False,
     use_fast_math: bool = False,
+    compute_comm_overlap: bool = True,
     **kwargs,  # other kwargs
 ):
     # Alway increase recompile_limit for dynamic shape compilation
@@ -42,9 +69,14 @@ def set_compile_configs(
 
     if dist.is_initialized():
         # Enable compute comm overlap
-        torch._inductor.config.reorder_for_compute_comm_overlap = True
+        torch._inductor.config.reorder_for_compute_comm_overlap = (
+            compute_comm_overlap and is_compile_compute_comm_overlap_enabled()
+        )
         # L20 64 GB/s, PCIe; A100/A800 NVLink 300 GB/s.
-        torch._inductor.config.intra_node_bw = 64 if "L20" in torch.cuda.get_device_name() else 300
+        if torch._inductor.config.reorder_for_compute_comm_overlap:
+            torch._inductor.config.intra_node_bw = (
+                64 if "L20" in torch.cuda.get_device_name() else 300
+            )
 
     if not descent_tuning:
         return
