b0e59e2fbc1463c344b43dd3c1b9d02fe0844448
DefTruth <qiustudent_r@163.com>
2025-12-09 06:57:36 +0000

    chore: use async all2all for default ulysses

--- src/cache_dit/parallelism/backends/native_diffusers/utils.py
+++ src/cache_dit/parallelism/backends/native_diffusers/utils.py
@@ -83,7 +83,7 @@ if (
             != _patch_find_submodule_by_name
         ):
             diffusers.hooks.context_parallel._find_submodule_by_name = _patch_find_submodule_by_name
-            logger.info("Patched _find_submodule_by_name for torch.nn.ModuleDict support.")
+            logger.info("Patched _find_submodule_by_name to support ModuleDict.")
 
 else:
 
