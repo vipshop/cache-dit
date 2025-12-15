3dd4c2948c95c4cb6d35053ee694a337c011cb3e
DefTruth <qiustudent_r@163.com>
2025-12-15 05:00:08 +0000

    refactor parallelism

--- examples/parallelism/run_flux_cp.py
+++ examples/parallelism/run_flux_cp.py
@@ -92,6 +92,8 @@ def run_pipe(pipe: FluxPipeline):
 if args.compile:
     cache_dit.set_compile_configs()
     pipe.transformer = torch.compile(pipe.transformer)
+    if args.compile_text_encoder:
+        pipe.text_encoder_2.encoder = torch.compile(pipe.text_encoder_2.encoder)
 
 # warmup
 _ = run_pipe(pipe)
