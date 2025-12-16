954d144c0f28318c47b9fbb901cf12b2d50c7501
DefTruth <qiustudent_r@163.com>
2025-12-16 07:05:34 +0000

    feat: support TP for many text encoder

--- src/cache_dit/parallelism/text_encoders/native_pytorch/tensor_parallelism/tp_planners.py
+++ src/cache_dit/parallelism/text_encoders/native_pytorch/tensor_parallelism/tp_planners.py
@@ -2,8 +2,13 @@
 def _activate_text_encoder_tp_planners():
     """Function to register all built-in tensor parallelism planners."""
     from .tp_plan_t5_encoder import T5EncoderTensorParallelismPlanner  # noqa: F401
+    from .tp_plan_umt5_encoder import UMT5EncoderTensorParallelismPlanner  # noqa: F401
     from .tp_plan_mistral3 import Mistral3TensorParallelismPlanner  # noqa: F401
     from .tp_plan_qwen2_5 import Qwen2_5_VLTensorParallelismPlanner  # noqa: F401
+    from .tp_plan_qwen3 import Qwen3TensorParallelismPlanner  # noqa: F401
+    from .tp_plan_llama import LlamaTensorParallelismPlanner  # noqa: F401
+    from .tp_plan_gemma import GemmaTensorParallelismPlanner  # noqa: F401
+    from .tp_plan_glm import GlmTensorParallelismPlanner  # noqa: F401
 
 
 __all__ = ["_activate_text_encoder_tp_planners"]
