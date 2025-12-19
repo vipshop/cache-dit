5d3af118030fd386b3193801d00f5cb9476daa0d
DefTruth <qiustudent_r@163.com>
2025-12-19 03:10:37 +0000

    wan vace example

--- examples/helpers.py
+++ examples/helpers.py
@@ -1,8 +1,8 @@
 def activate_all_examples():
     from registers import flux_example  # noqa: F403, F401
     from registers import flux2_example  # noqa: F403, F401
-    from registers import qwen_image_edit_example  # noqa: F403, F401
     from registers import qwen_image_example  # noqa: F403, F401
+    from registers import qwen_image_edit_example  # noqa: F403, F401
     from registers import skyreels_v2_example  # noqa: F403, F401
     from registers import wan_example  # noqa: F403, F401
     from registers import wan_vace_example  # noqa: F403, F401
