d162c312292ee349060c17ba0f8f276b9ae12511
DefTruth <qiustudent_r@163.com>
2025-12-25 07:24:50 +0000

    feat: support Qwen-Image-Edit-2511 & Qwen-Image-Edit-2511-Lightning

--- src/cache_dit/parallelism/transformers/native_diffusers/context_parallelism/cp_plan_qwen_image.py
+++ src/cache_dit/parallelism/transformers/native_diffusers/context_parallelism/cp_plan_qwen_image.py
@@ -118,7 +118,7 @@ class QwenImageContextParallelismPlanner(ContextParallelismPlanner):
             "proj_out": ContextParallelOutput(gather_dim=1, expected_dims=3),
         }
         if transformer.zero_cond_t:
-            # modulate_index: [b, l=seq_len]
+            # modulate_index: [b, l=seq_len], Qwen-Image-Edit-2511
             _cp_plan.update(
                 {
                     "transformer_blocks.*": {
