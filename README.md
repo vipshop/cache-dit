3295b52f9e07589451e87544277c68bfb2a638a1
DefTruth <qiustudent_r@163.com>
2025-12-25 08:11:51 +0000

    feat: support Qwen-Image-Layered

--- README.md
+++ README.md
@@ -82,6 +82,7 @@ You can install the stable release of cache-dit from PyPI, or the latest develop
 
 | 📚Supported DiTs: `🤗60+` | Cache  | C-P | T-P | TE-P | CN-P | VAE-P |
 |:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+| Qwen-Image-Layered | ✅ | ✅ | ✅ | ✅ | ✖️ | ✖️ |
 | Qwen-Image-Edit-2511-Lightning | ✅ | ✅ | ✅ | ✅ | ✖️ | ✖️ |
 | Qwen-Image-Edit-2511 | ✅ | ✅ | ✅ | ✅ | ✖️ | ✖️ |
 | LongCat-Image | ✅ | ✅ | ✅ | ✅ | ✖️ | ✖️ |
--- examples/helpers.py
+++ examples/helpers.py
@@ -4,6 +4,7 @@ def activate_all_examples():
     from registers import qwen_image_example  # noqa: F403, F401
     from registers import qwen_image_controlnet_example  # noqa: F403, F401
     from registers import qwen_image_edit_example  # noqa: F403, F401
+    from registers import qwen_image_layered_example  # noqa: F403, F401
     from registers import skyreels_v2_example  # noqa: F403, F401
     from registers import wan_example  # noqa: F403, F401
     from registers import wan_i2v_example  # noqa: F403, F401
