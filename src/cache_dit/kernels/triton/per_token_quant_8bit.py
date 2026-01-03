36f6a05db9b9dbabf8ce474eaa6c56fc2874fc64
triplemu <gpu@163.com>
2026-01-03 15:21:17 +0800

    support qkv permute+quant and dequant+permute fuse

--- src/cache_dit/kernels/triton/per_token_quant_8bit.py
+++ src/cache_dit/kernels/triton/per_token_quant_8bit.py
@@ -2,7 +2,12 @@ import torch
 import triton
 import triton.language as tl
 
-__all__ = ["per_token_quant_fp8", "per_token_dequant_fp8"]
+__all__ = [
+    "per_token_quant_fp8",
+    "per_token_dequant_fp8",
+    "qkv_permute_quant_fp8",
+    "qkv_dequant_permute_fp8",
+]
 
 
 @triton.jit
@@ -112,3 +117,144 @@ def per_token_dequant_fp8(x: torch.Tensor) -> torch.Tensor:
             num_warps=num_warps,
         )
     return y.reshape(*shape, H - 2)
+
+
+@triton.jit
+def _qkv_permute_quant(
+    quant_x_ptr: tl.tensor,
+    x_ptr: tl.tensor,
+    qx_stride_b: int,
+    qx_stride_n: int,
+    x_stride_b: int,
+    x_stride_s: int,
+    x_stride_p: int,
+    B: int,
+    S: int,
+    N: int,
+    D: int,
+    EPS: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_D: tl.constexpr,
+):
+    psb_id = tl.program_id(0).to(tl.int64)
+    b_id = psb_id % B
+    s_id = (psb_id // B) % S
+    p_id = psb_id // (S * B)
+
+    x_ptr += b_id * x_stride_b + s_id * x_stride_s + p_id * x_stride_p
+    quant_x_ptr += psb_id * qx_stride_b
+    scale_ptr = quant_x_ptr.to(tl.pointer_type(tl.float32, 1))
+
+    n_offset = tl.arange(0, BLOCK_SIZE_N)[None, :]
+    n_mask = n_offset < N
+    d_offset = tl.arange(0, BLOCK_SIZE_D)[:, None]
+    d_mask = d_offset < D
+    mask = n_mask & d_mask
+
+    quant_x_blk = quant_x_ptr + n_offset * qx_stride_n + d_offset
+    scale_blk = scale_ptr + n_offset * (D // 4 + 1) + D // 4
+    x_blk = x_ptr + n_offset * D + d_offset
+
+    x = tl.load(x_blk, mask=mask, other=0.0).to(tl.float32)
+    scale = tl.max(tl.abs(x), axis=0, keep_dims=True) / 448.0
+    scale = tl.maximum(scale, EPS)
+    quant_x = x / scale
+    quant_x = tl.clamp(quant_x, -448.0, 448.0).to(tl.float8e4nv)
+
+    tl.store(quant_x_blk, quant_x, mask=mask)
+    tl.store(scale_blk, scale, mask=n_mask)
+
+
+@triton.jit
+def _qkv_dequant_permute(
+    x_ptr: tl.tensor,
+    quant_x_ptr: tl.tensor,
+    x_stride_s: int,
+    qx_stride_s: int,
+    qx_stride_b: int,
+    qx_stride_n: int,
+    B: int,
+    S: int,
+    N: int,
+    D: int,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_D: tl.constexpr,
+):
+    bs_id = tl.program_id(0).to(tl.int64)
+    b_id = bs_id % B
+    s_id = bs_id // B
+
+    quant_x_ptr += s_id * qx_stride_s + b_id * qx_stride_b
+    scale_ptr = quant_x_ptr.to(tl.pointer_type(tl.float32, 1))
+    x_ptr += bs_id * x_stride_s
+
+    n_offset = tl.arange(0, BLOCK_SIZE_N)[None, :]
+    n_mask = n_offset < N
+    d_offset = tl.arange(0, BLOCK_SIZE_D)[:, None]
+    d_mask = d_offset < D
+    mask = n_mask & d_mask
+
+    x_blk = x_ptr + n_offset * D + d_offset
+    quant_x_blk = quant_x_ptr + n_offset * qx_stride_n + d_offset
+    scale_blk = scale_ptr + n_offset * (D // 4 + 1) + D // 4
+
+    qx = tl.load(quant_x_blk, mask=mask, other=0.0).to(tl.float32)
+    scale = tl.load(scale_blk, mask=n_mask, other=0.0).to(tl.float32)
+
+    tl.store(x_blk, qx * scale, mask=mask)
+
+
+def qkv_permute_quant_fp8(x: torch.Tensor, eps: float = 1e-6) -> torch.Tensor:
+    B, S, P, N, D = x.shape
+
+    quant_x = torch.empty((P, S, B, N, D + 4), dtype=torch.float8_e4m3fn, device=x.device)
+
+    grid = (P * S * B,)
+
+    with torch.cuda.device(x.device):
+        _qkv_permute_quant[grid](
+            quant_x,
+            x,
+            quant_x.stride(2),
+            quant_x.stride(3),
+            x.stride(0),
+            x.stride(1),
+            x.stride(2),
+            B,
+            S,
+            N,
+            D,
+            eps,
+            triton.next_power_of_2(N),
+            triton.next_power_of_2(D),
+        )
+
+    return quant_x
+
+
+def qkv_dequant_permute_fp8(
+    quant_x: torch.Tensor, dtype: torch.dtype = torch.bfloat16
+) -> torch.Tensor:
+    S, B, N, _D = quant_x.shape
+    D = _D - 4
+    x = torch.empty((B, S, N, D), dtype=dtype, device=quant_x.device)
+
+    grid = (B * S,)
+
+    with torch.cuda.device(x.device):
+        _qkv_dequant_permute[grid](
+            x,
+            quant_x,
+            x.stride(1),
+            quant_x.stride(0),
+            quant_x.stride(1),
+            quant_x.stride(2),
+            B,
+            S,
+            N,
+            D,
+            triton.next_power_of_2(N),
+            triton.next_power_of_2(D),
+        )
+
+    return x
