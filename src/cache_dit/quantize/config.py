6a77c52357981673309a295ce58aae6d7022f50f
DefTruth <qiustudent_r@163.com>
2026-03-13 10:57:11 +0000

    feat: expand quantize config

--- src/cache_dit/quantize/config.py
+++ src/cache_dit/quantize/config.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List, Union
 from cache_dit.logger import init_logger
 
 logger = init_logger(__name__)
@@ -16,6 +16,14 @@ class QuantizeConfig:
         default_factory=lambda: ["embedder", "embed"]
     )
     filter_fn: Optional[Any] = None  # type: ignore
+    # components_to_quantize: (list[str] or dict[str, str], optional)
+    # specify the components to quantize, if None, only the transformer
+    # module will be quantized. e.g:
+    # - List[str]: ['transformer', 'text_encoder'] quantize to 'quant_type'
+    # - Dict[str, str]: {'transformer': 'float8', 'text_encoder': 'float8_weight_only'}.
+    #   The 'quant_type' will be ignored in this case, each module will quantized to
+    #   it's specified quantization type.
+    components_to_quantize: Optional[Union[List[str], Dict[str, str]]] = None
     verbose: bool = False
 
     def as_dict(self) -> Dict[str, Any]:
@@ -30,3 +38,26 @@ class QuantizeConfig:
 
     def strify(self) -> str:
         return f"{self.quant_type.lower()}"
+
+    # Transformer components_to_quantize to mutiple simple configs, each
+    # with only one component to quantize, and the same quantization type.
+    @classmethod
+    def expand_configs(cls, config: "QuantizeConfig") -> List["QuantizeConfig"]:
+        if config.components_to_quantize is None:
+            return [config]
+
+        if isinstance(config.components_to_quantize, list):
+            return [
+                dataclasses.replace(config, components_to_quantize=[component])
+                for component in config.components_to_quantize
+            ]
+
+        if isinstance(config.components_to_quantize, dict):
+            return [
+                dataclasses.replace(
+                    config, components_to_quantize=[component], quant_type=quant_type
+                )
+                for component, quant_type in config.components_to_quantize.items()
+            ]
+
+        raise ValueError("components_to_quantize should be either a list or a dict.")
