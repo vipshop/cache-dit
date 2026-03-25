fdd16df5589ab407f44ead326f5a839ea93f3e72
DefTruth <31974251+DefTruth@users.noreply.github.com>
2026-03-25 10:07:59 +0800

    kernel: refactor custom triton kernels (#907)
    * refactor custom triton kernels
    
    * refactor custom triton kernels

--- src/cache_dit/kernels/__init__.py
+++ src/cache_dit/kernels/__init__.py
@@ -1,7 +1,45 @@
-from .triton import (
-    fp8_comm_per_token_quant,
-    fp8_comm_per_token_dequant,
-    fp8_comm_qkv_permute_quant,
-    fp8_comm_qkv_permute_dequant,
-    fused_merge_attn_states,
+# Currently we only have triton kernels for cache-dit, but we can re-implement
+# some of them in CuteDSL for better performance if needed in the future. In
+# that case, we can keep the same operator definitions in this file and just
+# change the underlying implementations.
+
+import torch
+from typing import Tuple
+
+from .triton_ops_registery import (
+    triton_fp8_comm_per_token_quant,
+    triton_fp8_comm_per_token_dequant,
+    triton_fp8_comm_qkv_permute_quant,
+    triton_fp8_comm_qkv_permute_dequant,
+    triton_fused_merge_attn_states,
 )
+
+
+def fp8_comm_per_token_quant(x: torch.Tensor) -> torch.Tensor:
+    return triton_fp8_comm_per_token_quant(x)
+
+
+def fp8_comm_per_token_dequant(x: torch.Tensor) -> torch.Tensor:
+    return triton_fp8_comm_per_token_dequant(x)
+
+
+def fp8_comm_qkv_permute_quant(x: torch.Tensor) -> torch.Tensor:
+    return triton_fp8_comm_qkv_permute_quant(x)
+
+
+def fp8_comm_qkv_permute_dequant(quant_x: torch.Tensor) -> torch.Tensor:
+    return triton_fp8_comm_qkv_permute_dequant(quant_x)
+
+
+def fused_merge_attn_states(
+    prev_out: torch.Tensor,
+    prev_lse: torch.Tensor,
+    suff_out: torch.Tensor,
+    suff_lse: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    return triton_fused_merge_attn_states(
+        prev_out,
+        prev_lse,
+        suff_out,
+        suff_lse,
+    )
--- src/cache_dit/kernels/triton/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ops_registery import (
-    fp8_comm_per_token_quant,
-    fp8_comm_per_token_dequant,
-    fp8_comm_qkv_permute_quant,
-    fp8_comm_qkv_permute_dequant,
-    fused_merge_attn_states,
-)
