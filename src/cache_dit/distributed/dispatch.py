a45dc3bed7279a560dae9e3e15975e09d9287ce4
DefTruth <qiustudent_r@163.com>
2026-04-10 12:54:09 +0000

    bc: refactor distributed codebase

--- src/cache_dit/distributed/dispatch.py
+++ src/cache_dit/distributed/dispatch.py
@@ -189,8 +189,7 @@ def _maybe_set_module_attention_backend(
     else:
       # Ensure custom attention backends are registered in cache-dit.
       if resolved_cache_dit_backend is not None and not ENV.CACHE_DIT_CUSTOM_ATTN_ALREADY_DISPATCH:
-        from ..attention import (
-          _maybe_register_custom_attn_backends, )
+        from ..attention import _maybe_register_custom_attn_backends
 
         _maybe_register_custom_attn_backends()
 
