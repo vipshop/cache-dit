6e2c2f221e276d6dcd75186446e0ca3ac91a6496
DefTruth <qiustudent_r@163.com>
2026-04-13 05:44:58 +0000

    kernel: add cute-dsl based merge-attn-states kernel

--- src/cache_dit/distributed/core/__init__.py
+++ src/cache_dit/distributed/core/__init__.py
@@ -1,7 +1,7 @@
-from ._context_parallel import (
-  _get_submodule_by_name, )
+from ._context_parallel import _get_submodule_by_name
 from ._distributed_primitives import (
   _All2AllComm,
+  _RingP2PComm,
   _all_to_all_o_async_fn,
   _all_to_all_qkv_async_fn,
   _init_comm_metadata,
@@ -16,8 +16,7 @@ from ._modeling_parallel import (
   _ContextParallelOutput,
   _ContextParallelConfig,
 )
-from ._runtime import (
-  _enable_context_parallelism, )
+from ._runtime import _enable_context_parallelism
 from ._templated_ring import RingAttention
 from ._templated_ulysses import UlyssesAttention
 from ._templated_usp import USPAttention
@@ -28,6 +27,7 @@ __all__ = [
   "_ContextParallelOutput",
   "_ContextParallelModelPlan",
   "_All2AllComm",
+  "_RingP2PComm",
   "_get_submodule_by_name",
   "_normalize_parallel_config",
   "_enable_context_parallelism",
